6fb271b6671ff3f2d75a020409d6f530e0956795
leonyoliveir <leony.oliveira.bernardo@gmail.com>
2020-03-03 09:20:02 -0300

    erro nulo em regime permanente

--- ADRC Primeira Ordem Simulink/preload.m
+++ ADRC Primeira Ordem Simulink/preload.m
@@ -1,11 +1,11 @@
-num = [1];
-den = [1, 3];
+num = [100];
+den = [1, -5];
 
-ts = 4/den(2);
-kp = 4/ts;
-b = num * den(2);
+ts = 4 / den(2);
+kp = 4 / ts;
+b = num;
 
-set_param('ADRC/Kp', 'Gain', num2str(kp))
-set_param('ADRC/B', 'Gain', num2str(1/b))
-set_param('ADRC/Plant', 'Numerator', num2str(num))
-set_param('ADRC/Plant', 'Denominator', strcat('[',num2str(den),']'))
+set_param('ADRC_14a/Kp', 'Gain', num2str(kp))
+set_param('ADRC_14a/B', 'Gain', num2str(1/b))
+set_param('ADRC_14a/Plant', 'Numerator', num2str(num))
+set_param('ADRC_14a/Plant', 'Denominator', strcat('[',num2str(den),']'))
